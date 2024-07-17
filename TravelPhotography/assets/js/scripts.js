6632e82dce8cd4208bf4748d965792376b89fadf
Yuhan Zhu <zhuyuha3@msu.edu>
2024-07-17 17:17:42 -0400

    fix bug

--- TravelPhotography/assets/js/scripts.js
+++ TravelPhotography/assets/js/scripts.js
@@ -1,21 +1,9 @@
-function showPage(pageId) {
-    const sections = document.querySelectorAll('main section');
-    sections.forEach(section => {
-        if (section.id === pageId) {
-            section.classList.remove('hidden');
-        } else {
-            section.classList.add('hidden');
-        }
-    });
-}
-
-// Show the home page by default
-document.addEventListener('DOMContentLoaded', function() {
-    showPage('home');
-});
-
 function toggleMenu() {
     const menu = document.querySelector('nav ul');
     menu.classList.toggle('menu-active');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    toggleMenu(); // 初始化菜单状态
+});
 
