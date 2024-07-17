64416d0599e27e5845e46b9f27be6d05f2614eb4
Yuhan Zhu <zhuyuha3@msu.edu>
2024-07-17 15:33:04 -0400

    add home button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,3 +19,33 @@ function toggleMenu() {
     menu.classList.toggle('menu-active');
 }
 
+function calculatePrice() {
+    const milkType = document.getElementById('milk-type').value;
+    const quantity = parseInt(document.getElementById('quantity').value, 10);
+    let price = 0;
+    switch (milkType) {
+        case 'Whole Milk':
+        case '2%Milk':
+            price = 0.2;
+            break;
+        case 'Oat Milk':
+        case 'Almond Milk':
+            price = 0.3;
+            break;
+        case 'Lactose Free Milk':
+        case 'Coconut Milk':
+            price = 0.4;
+            break;
+    }
+    const totalPrice = (price * quantity).toFixed(2);
+    document.getElementById('price').innerText = `Total Price: $${totalPrice}`;
+    document.getElementById('joke-section').style.display = 'block';
+}
+
+function handleJoke() {
+    const jokeSelect = document.getElementById('joke');
+    const selectedJoke = jokeSelect.options[jokeSelect.selectedIndex].text;
+    document.getElementById('price').innerText += `\n${selectedJoke}`;
+    document.getElementById('payment-buttons').style.display = 'block';
+}
+
